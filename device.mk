abdd6c308a5e1e60f314bcc0d97f88b3d87192c0
trpsl <trpsl755@gmail.com>
2020-03-24 14:10:26 +0100

    vayu: Add Moto Dolby
    Values taken from OP9 Pro and modified by Sasha (@helenius147 on Telegram)
    to work with Moto Dolby.
    
    - Additionally remove dax-default.xml from proprietary-files.txt
    - Improve audio_effects.xml changes
    - Move props from product.prop to vendor.prop
    - Dolby: We have 2 Output channels for Speakers
    - Disable AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP
    - ro.control_privapp_permissions=log -> force
    - Remove media_codecs_dolby_audio.xml, because we are not having backend for this.
    - Remove libswvqe Deps, they are not existing here for MotoDolby
    - Adapt for Enforcing A12.1 Sepolicy
    - Add # Dolby declarations for Sepolicy
    
    Co-Authored-By: blankaf <realblankaf@gmail.com>
    Co-Authored-By: MB <mb150996@gmail.com>
    Co-Authored-By: johnmart19 <johnivan19999@gmail.com>

--- device.mk
+++ device.mk
@@ -104,6 +104,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
 
+# Audio Motodolby
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    persist.vendor.audio_fx.current=dolby \
+    ro.vendor.dolby.dax.version=DS1_2.2.0.0_r1
+
+# Audio Motodolby Config
+PRODUCT_COPY_FILES += \
+    device/xiaomi/haydn/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
