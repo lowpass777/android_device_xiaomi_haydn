374153f1f70b2e6fc427dc0e254aa084a5d96c90
lowpass777 <eefvanschaik@live.nl>
2022-07-26 22:47:05 +0200

    remove schedtune entries

--- rootdir/bin/init.kernel.post_boot-lahaina.sh
+++ rootdir/bin/init.kernel.post_boot-lahaina.sh
@@ -95,6 +95,34 @@ echo 4-6 > /dev/cpuset/foreground/boost/cpus
 echo 0-7 > /dev/cpuset/top-app/cpus
 echo 0-3 > /dev/cpuset/restricted/cpus
 
+#Uclamp tuning
+sysctl -w kernel.sched_util_clamp_min_rt_default=96
+sysctl -w kernel.sched_util_clamp_min=128
+
+#top-app
+echo max > /dev/cpuset/top-app/uclamp.max
+echo 20  > /dev/cpuset/top-app/uclamp.min
+echo 1   > /dev/cpuset/top-app/uclamp.boosted
+echo 1   > /dev/cpuset/top-app/uclamp.latency_sensitive
+
+#foreground
+echo 50 > /dev/cpuset/foreground/uclamp.max
+echo 20 > /dev/cpuset/foreground/uclamp.min
+echo 0  > /dev/cpuset/foreground/uclamp.boosted
+echo 0  > /dev/cpuset/foreground/uclamp.latency_sensitive
+
+#background
+echo max > /dev/cpuset/background/uclamp.max
+echo 20  > /dev/cpuset/background/uclamp.min
+echo 0   > /dev/cpuset/background/uclamp.boosted
+echo 0   > /dev/cpuset/background/uclamp.latency_sensitive
+
+#system-background
+echo 50 > /dev/cpuset/system-background/uclamp.max
+echo 10 > /dev/cpuset/system-background/uclamp.min
+echo 0  > /dev/cpuset/system-background/uclamp.boosted
+echo 0  > /dev/cpuset/system-background/uclamp.latency_sensitive
+
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/kernel/sched_boost
 
